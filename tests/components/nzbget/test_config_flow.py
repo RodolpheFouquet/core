f3ed5e51320fa59ca94f8f0bdda9b7648f5e40bb
Chris Talkington <chris@talkingtontech.com>
2020-08-17 23:39:57 -0500

    Update test_config_flow.py

--- tests/components/nzbget/test_config_flow.py
+++ tests/components/nzbget/test_config_flow.py
@@ -11,7 +11,13 @@ from homeassistant.data_entry_flow import (
 )
 from homeassistant.setup import async_setup_component
 
-from . import ENTRY_CONFIG, MOCK_HISTORY, MOCK_STATUS, MOCK_VERSION
+from . import (
+    ENTRY_CONFIG,
+    MOCK_HISTORY,
+    MOCK_STATUS,
+    MOCK_VERSION,
+    USER_INPUT,
+)
 
 from tests.async_mock import patch
 from tests.common import MockConfigEntry
@@ -29,12 +35,12 @@ async def test_form(hass):
 
     with _patch_version(), _patch_status(), _patch_history(), _patch_async_setup() as mock_setup, _patch_async_setup_entry() as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], ENTRY_CONFIG,
+            result["flow_id"], USER_INPUT,
         )
 
         assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
         assert result2["title"] == "10.10.10.30"
-        assert result2["data"] == {**ENTRY_CONFIG, CONF_VERIFY_SSL: False}
+        assert result2["data"] == {**USER_INPUT, CONF_VERIFY_SSL: False}
 
         await hass.async_block_till_done()
         assert len(mock_setup.mock_calls) == 1
@@ -52,7 +58,7 @@ async def test_user_form_cannot_connect(hass):
         side_effect=NZBGetAPIException(),
     ):
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], ENTRY_CONFIG,
+            result["flow_id"], USER_INPUT,
         )
 
     assert result2["type"] == RESULT_TYPE_FORM
@@ -69,7 +75,7 @@ async def test_user_form_unexpected_exception(hass):
         "homeassistant.components.nzbget.NZBGetAPI.version", side_effect=Exception(),
     ):
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], ENTRY_CONFIG,
+            result["flow_id"], USER_INPUT,
         )
 
     assert result2["type"] == RESULT_TYPE_ABORT
@@ -82,7 +88,7 @@ async def test_user_form_single_instance_allowed(hass):
     entry.add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}, data=ENTRY_CONFIG,
+        DOMAIN, context={"source": SOURCE_USER}, data=USER_INPUT,
     )
     assert result["type"] == RESULT_TYPE_ABORT
     assert result["reason"] == "single_instance_allowed"
