da29cedd3dafe1e22b97b7d5105d85afe540984a
Chris Talkington <chris@talkingtontech.com>
2020-08-17 22:48:59 -0500

    Update __init__.py

--- homeassistant/components/nzbget/__init__.py
+++ homeassistant/components/nzbget/__init__.py
@@ -74,6 +74,8 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
         return True
 
     if DOMAIN in config:
+        config[DOMAIN][CONF_VERIFY_SSL] = DEFAULT_VERIFY_SSL
+
         hass.async_create_task(
             hass.config_entries.flow.async_init(
                 DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN],
